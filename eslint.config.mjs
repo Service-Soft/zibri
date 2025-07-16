86d8180493d8230867acdd8837bcbfbe8684b3e7
Tim Fabian <fabian@service-soft.de>
2025-07-16 18:04:22 +0200

    removed docs from linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import { configs } from 'eslint-config-service-soft';
 /** @type {import('eslint').Linter.Config} */
 export default [
     ...configs,
-    { ignores: ['tsconfig.json', 'tsup.config.ts', 'sandbox'] },
+    { ignores: ['tsconfig.json', 'tsup.config.ts', 'sandbox', 'docs'] },
     {
         files: ['*.js', '*.mjs'],
         rules: {
